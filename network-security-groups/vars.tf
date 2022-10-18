59fbbf720f05cd829367b6a872c428ae3444a17f
Fahad Ghonaim <abumoath30@hotmail.com>
2022-10-18 08:04:29 -0400

    adding azure terraform files

---
+++ network-security-groups/vars.tf
@@ -0,0 +1,14 @@
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
+
+variable "prefix" {
+  type    = string
+  default = "demo"
+}
+
+variable "ssh-source-address" {
+  type    = string
+  default = "142.126.238.44/32"
+}
